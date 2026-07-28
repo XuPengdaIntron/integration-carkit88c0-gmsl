74e98a97ad6aaba77895a66b29e6e04a3801cf46
user <user@tegra-ubuntu>
2026-07-28 04:41:31 +0000

    Fix path in 04_run_radar.sh from ./ to ../ for correct directory navigation

--- GDCU4XL/04_run_radar.sh
+++ GDCU4XL/04_run_radar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./integration-carkit88c0-gmsl/src/demo_app/boards/carkit_88c0/example
+cd ../integration-carkit88c0-gmsl/src/demo_app/boards/carkit_88c0/example
 make clean
 make
 sudo ./bin/carkit88c0_gmsl_example  
